478f5994618c618ea35e84c7597ef949c093a00c
Tom Wright <contact@tomwright.me>
2018-04-30 08:57:36 +0100

    Race condition changes:
    - Pool public funcs now lock using a mutex
    - Restrict which pool funcs are publicly available
    - Adjust tests so they are appropriate

--- pool_test.go
+++ pool_test.go
@@ -7,7 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPool_Start(t *testing.T) {
+// Test that the desired process count is acknowledged
+func TestPool_SetDesiredProcessCount(t *testing.T) {
 	a := assert.New(t)
 
 	// we have a list of names and we want to print them to the screen
@@ -33,106 +34,40 @@ func TestPool_Start(t *testing.T) {
 		}
 		return nil
 	})
-
+	p.SetProcessManagerTicker(time.NewTicker(time.Millisecond * 400))
+	p.SetDesiredProcessCount(func(pool *Pool) uint64 { return 1 })
 	a.Equal(0, p.ProcessCount())
 
 	// start the pool
-	p.Start()
-	time.Sleep(time.Millisecond * 20)
+	err := p.Start()
+	a.NoError(err)
+	time.Sleep(time.Millisecond * 600)
 	a.Equal(1, p.ProcessCount())
 
-	p.AddProcess()
-	time.Sleep(time.Millisecond * 20)
+	p.SetDesiredProcessCount(func(pool *Pool) uint64 { return 2 })
+	time.Sleep(time.Millisecond * 600)
 	a.Equal(2, p.ProcessCount())
 
-	p.AddProcess()
-	p.AddProcess()
-	time.Sleep(time.Millisecond * 20)
+	p.SetDesiredProcessCount(func(pool *Pool) uint64 { return 4 })
+	time.Sleep(time.Millisecond * 600)
 	a.Equal(4, p.ProcessCount())
 
-	p.RemoveProcess(p.Processes()[2])
-	time.Sleep(time.Millisecond * 20)
+	p.SetDesiredProcessCount(func(pool *Pool) uint64 { return 3 })
+	time.Sleep(time.Millisecond * 600)
 	a.Equal(3, p.ProcessCount())
 
-	p.ensureProcessCount()
-	time.Sleep(time.Millisecond * 20)
+	p.SetDesiredProcessCount(func(pool *Pool) uint64 { return 1 })
+	time.Sleep(time.Millisecond * 600)
 	a.Equal(1, p.ProcessCount())
 
-	p.Stop()
-	time.Sleep(time.Millisecond * 20)
-	a.Equal(0, p.ProcessCount())
-}
-
-func TestPool_EnsureProcessCountDuration(t *testing.T) {
-	a := assert.New(t)
-
-	// we have a list of names and we want to print them to the screen
-	names := make(chan string, 10)
-
-	var count uint64 = 1
-	desiredCount := &count
-
-	// create a process to do so
-	p := NewPool("name-printer", func(process *Process, commands <-chan ProcessCommand) error {
-		// inside our func, we want to keep running forever, until either:
-		// - the names channel is closed
-		// - a stop command is passed in
-		for {
-			select {
-			case cmd := <-commands: // take a command from the commands channel
-				if cmd == StopProcessCommand {
-					return nil
-				}
-			case name, open := <-names: // take a name from the names channel
-				if ! open {
-					return nil
-				}
-				fmt.Println(process.ID(), name)
-			}
-		}
-		return nil
-	}).
-		SetEnsureProcessCountDuration(time.Millisecond * 100).
-		SetDesiredProcessCount(func(pool *Pool) uint64 {
-		return *desiredCount
-	})
-
+	err = p.Stop()
+	a.NoError(err)
+	time.Sleep(time.Millisecond * 600)
 	a.Equal(0, p.ProcessCount())
 
-	// start the pool
-	p.Start()
-	time.Sleep(time.Millisecond * 20)
-	a.Equal(1, p.ProcessCount())
-
-	count = 2
-	time.Sleep(time.Millisecond * 500)
-	a.Equal(2, p.ProcessCount())
-
-	count = 4
-	time.Sleep(time.Millisecond * 500)
-	a.Equal(4, p.ProcessCount())
-
-	count = 0
-	time.Sleep(time.Millisecond * 500)
-	a.Equal(0, p.ProcessCount())
-
-	count = 1
-	time.Sleep(time.Millisecond * 500)
-	a.Equal(1, p.ProcessCount())
-
-	p.Stop()
-	time.Sleep(time.Millisecond * 20)
-	a.Equal(0, p.ProcessCount())
-
-	count = 10
-	time.Sleep(time.Millisecond * 500)
-	a.Equal(0, p.ProcessCount())
-
-	p.Start()
-	time.Sleep(time.Millisecond * 20)
-	a.Equal(10, p.ProcessCount())
-
-	p.Stop()
-	time.Sleep(time.Millisecond * 20)
-	a.Equal(0, p.ProcessCount())
+	p.SetDesiredProcessCount(func(pool *Pool) uint64 { return 5 })
+	err = p.Start()
+	a.NoError(err)
+	time.Sleep(time.Millisecond * 600)
+	a.Equal(5, p.ProcessCount())
 }
